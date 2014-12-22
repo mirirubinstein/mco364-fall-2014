476a2397d725ac432c7cb00e25e805fe054d4779
mirirubinstein <mirirubinstein@yahoo.com>
2014-12-21 19:55:27 -0500

    added layers

--- src/rubinstein/paint/BrushPanel.java
+++ src/rubinstein/paint/BrushPanel.java
@@ -12,27 +12,45 @@ import javax.swing.JPanel;
 public class BrushPanel extends JPanel {
 	private JPanel colorPanel;
 	private JPanel strokePanel;
+	private JPanel layerPanel;
 	private JLabel strokeLabel;
 	private JLabel strokeTitle;
+	private JLabel currentLayer;
+	private JLabel currentLayerLabel;
 	private int strokeWidth;
+	private int currentLayerNum;
 
 	public BrushPanel() {
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		setSize(200, 600);
 
 		colorPanel = new JPanel();
+		
 		colorPanel.setBackground(Color.BLACK);
 		add(colorPanel);
 		strokeWidth = 10;
+		currentLayerNum = 1;
 		strokePanel = new JPanel();
 		strokePanel.setLayout(new BoxLayout(strokePanel, BoxLayout.Y_AXIS));
 		strokeLabel = new JLabel("" + strokeWidth);
-		strokeLabel.setFont(new Font("Helvetica", Font.PLAIN, 150));
+		strokeLabel.setFont(new Font("Helvetica", Font.PLAIN, 100));
 		strokeTitle = new JLabel("Stroke width: ");
 		strokeTitle.setFont(new Font("Helvetica", Font.PLAIN, 20));
+		
 		strokePanel.add(strokeTitle);
 		strokePanel.add(strokeLabel);
 		add(strokePanel);
+		
+		layerPanel = new JPanel();
+		layerPanel.setLayout(new BoxLayout(layerPanel, BoxLayout.Y_AXIS));
+		currentLayer = new JLabel("Current Layer: ");
+		currentLayer.setFont(new Font("Helvetica", Font.PLAIN, 20));
+		currentLayerLabel = new JLabel("" + currentLayerNum);
+		currentLayerLabel.setFont(new Font("Helvetica", Font.PLAIN, 100));
+		layerPanel.add(currentLayer);
+		layerPanel.add(currentLayerLabel);
+		add(layerPanel);
+		
 
 		
 	}
@@ -45,10 +63,16 @@ public class BrushPanel extends JPanel {
 		strokeWidth = strokeThickness;
 		repaint();
 	}
+	public void setCurrentLayer(int currentLayer){
+		currentLayerNum = currentLayer + 1;
+		repaint();
+	}
 
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		strokeLabel.setText("" + strokeWidth);
+		currentLayerLabel.setText("" + currentLayerNum);
+		
 	}
 }
