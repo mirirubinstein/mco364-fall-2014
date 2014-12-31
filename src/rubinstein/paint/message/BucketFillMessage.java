80dd8db883e3816bc34aceb54585c9eda9f9624e
mirirubinstein <mirirubinstein@yahoo.com>
2014-12-30 20:56:09 -0500

    bucket fill works with network

--- src/rubinstein/paint/message/BucketFillMessage.java
+++ src/rubinstein/paint/message/BucketFillMessage.java
@@ -1,13 +1,22 @@
 package rubinstein.paint.message;
 
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.util.Stack;
+
+import rubinstein.paint.Canvas;
+
 
 public class BucketFillMessage implements PaintMessage {
+
+	private Canvas canvas; 
 	private int x;
 	private int y;
 	private int color;
 
-	public BucketFillMessage(int x, int y, int color) {
+	public BucketFillMessage(Canvas canvas,int x, int y, int color) {
+		this.canvas = canvas;
 		this.x = x;
 		this.y = y;
 		this.color = color;
@@ -38,14 +47,49 @@ public class BucketFillMessage implements PaintMessage {
 	}
 
 	@Override
-	public String toString() {
+	public String toString(){
 		return "BUCKET_FILL " + x + " " + y + " " + color + "\n";
 	}
 
 	@Override
 	public void apply(Graphics2D g) {
 		// TODO Auto-generated method stub
+		BufferedImage bufferedImage = (BufferedImage) canvas.getImage();
+        int clickedColor = bufferedImage.getRGB(x, y);
+        int newColor = canvas.getColor().getRGB();
+
+		floodFill4Stack(x,y, newColor, clickedColor, bufferedImage);
 
 	}
+    void floodFill4Stack(int x, int y, int newColor, int oldColor, BufferedImage bufferedImage) {
+        Stack<Point> stack = new Stack<Point>();
+        stack.push(new Point(x, y));
+        if (newColor == oldColor)
+            return; // avoid infinite loop
+        while (!stack.isEmpty()) {
+            
+            Point current = stack.pop();
+                    //    System.out.println(current);
+                        bufferedImage.setRGB((int) current.getX(), (int) current.getY(),
+                                newColor);
+
+                        if (current.getX() + 1 < bufferedImage.getWidth()
+                                && bufferedImage.getRGB((int)current.getX() + 1, (int)current.getY()) == oldColor) {
+                            stack.push(new Point((int)current.getX() + 1, (int)current.getY()));
+                        }
+                        if (current.getX() - 1 >= 0 && bufferedImage.getRGB((int)current.getX() - 1, (int)current.getY()) == oldColor) {
+                            stack.push(new Point((int)current.getX() - 1, (int)current.getY()));
+                        }
+                        if (current.getY() + 1 < bufferedImage.getHeight()
+                                && bufferedImage.getRGB((int)current.getX(), (int)current.getY() + 1) == oldColor) {
+                            stack.push(new Point((int)current.getX(), (int)current.getY() + 1));
+                        }
+                        if (current.getY() - 1 >= 0 && bufferedImage.getRGB((int)current.getX(), (int)current.getY() - 1) == oldColor) {
+                            stack.push(new Point((int)current.getX(), (int)current.getY() - 1));
+                        }
+                    }
+       // canvas.repaint();
+    }
+
 
 }
